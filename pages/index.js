40cf24241bda4aad3990bb00226d91432fb35c70
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-10-17 21:48:43 +0900

    20231017

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }

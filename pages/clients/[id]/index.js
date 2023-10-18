eb09854a040990d0f7a51adc65ac1cb8736c1b47
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-10-18 21:50:05 +0900

    20231018

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,14 @@ import { useRouter } from "next/router";
 export default function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
+
+  const loadProjectHandler = () => {
+    router.replace("/clients/max/projectA"); // push는 이전 페이지로 이동할 수 있고 replace는 이전 페이지로 못돌아감
+  };
   return (
     <div>
       <h1>The Project of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 export default function ClientPage() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "menu", name: "Menuel" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((item) => {
+          return (
+            <li key={item.name}>
+              <Link
+                href={{
+                  pathname: "/clients/[id]",
+                  query: { id: item.id },
+                }}
+              >
+                {item.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }

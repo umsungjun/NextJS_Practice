b46f71eaa28640e2a1f9d1395abffaa7a6068fa3
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-11-13 20:46:07 +0900

    current

--- pages/about/index.js
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>The Home Page</h1>
-    </div>
-  );
-}
--- pages/portfolio/list.js
+++
@@ -1,7 +0,0 @@
-export default function ListPage() {
-  return (
-    <div>
-      <h1>Teh List Page</h1>
-    </div>
-  );
-}

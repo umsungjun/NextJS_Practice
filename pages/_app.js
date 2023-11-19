37046c5666f06381e18a27dddd95262d7c15f408
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-11-19 21:44:22 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <span>hello</span>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
